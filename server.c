e956cbe8fab788e88c15e99c6a66aacbebd51d78
Álvaro Torralba <donfrutosgomez@gmail.com>
2018-09-11 20:03:49 +0000

    Update server.c

--- server.c
+++ server.c
@@ -35,19 +35,19 @@ int main(int argc, char *argv[])
 	int stsocket, new_stsocket, stport, n;
 	socklen_t clilen;
 	char buffer[2048];
-	struct sockaddr_in addres, cli_addr;
+	struct sockaddr_in address, cli_addr;
 	if (argc == 2 && strcmp(argv[1], "-h") == 0 || argc == 2 && strcmp(argv[1], "--help") == 0)
 	{
 		printf("Uso: %s [opciones]\nOpciones:\n\t-h, --help\tMuestra esta ayuda.\n\nEstado de salida:\n\t0 si todo fue bien.\n\t1 si el puerto necesario no está disponible.\n\t2 si no se pudo escribir o leer el socket.\n", argv[0]);
 		return 0;
 	}
 	stsocket = socket(AF_INET, SOCK_STREAM, 0);
-	bzero((char *) &addres, sizeof(addres));
+	bzero((char *) &address, sizeof(address));
 	stport = 3890;
 	addres.sin_family = AF_INET;
 	addres.sin_addr.s_addr = htonl(INADDR_ANY);
 	addres.sin_port = htons(stport);
-	n = bind(stsocket, (struct sockaddr *) &addres, sizeof(addres)); 
+	n = bind(stsocket, (struct sockaddr *) &address, sizeof(address)); 
 	if (n < 0)
 	{
 		write(new_stsocket, "202 - El puerto necesario no está disponible.\n", 56);
